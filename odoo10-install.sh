5d5ff5428103329f402363265a1ad42f797b597c
fgomezk <fgomezk@gmail.com>
2017-11-16 19:01:04 +0100

    Simplify installation
    Remove creation of folders and so on

--- odoo10-install.sh
+++ odoo10-install.sh
@@ -14,12 +14,5 @@ sudo npm install -g less less-plugin-clean-css #Install less compiler
 
 # wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.1/wkhtmltox-0.12.1_linux-trusty-amd64.deb
 
-sudo cp -Ra * /home/odoo/
-
-cd /home/odoo/odoo-dev
-sudo chmod +x ./odoo10-base-install.sh
 sudo ./odoo10-base-install.sh
-
-cd /home/odoo/odoo-dev
-sudo chmod +x ./odoo10-extras.sh
 sudo ./odoo10-extras.sh
