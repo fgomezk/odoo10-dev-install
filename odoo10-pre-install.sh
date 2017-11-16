7eddf46333eb11730e37153f80ffd2a6995d70f8
fgomezk <fgomezk@gmail.com>
2017-11-16 11:55:45 +0100

    Update odoo10-pre-install.sh

--- odoo10-pre-install.sh
+++ odoo10-pre-install.sh
@@ -10,6 +10,6 @@ sudo apt-get -y install npm #Install NodeJs and its package manager
 sudo ln -s /usr/bin/nodejs/usr/bin/node # call node runs nodejs
 sudo npm install -g less less-plugin-clean-css #Install less compiler
 
-# wkhtmltopdf: Verificar si hace falta para Odoo 10.0
+# wkhtmltopdf: Verify if it's needed for Odoo 10.0
 
 # wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.1/wkhtmltox-0.12.1_linux-trusty-amd64.deb
