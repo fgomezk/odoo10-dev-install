c959b8268e3b259770cab05e1296476678659f5e
fgomezk <fgomezk@gmail.com>
2017-11-16 11:12:09 +0100

    Create odoo10-pre-install.sh

---
+++ odoo10-pre-install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Instalar PreOdoo 10
+
+useradd -m -g sudo -s /bin/bash odoo  # Create an 'odoo' user with sudo powers
+passwd odoo  # Ask and set a password for the new user
+
+sudo apt-get -y install git # Install Git
+sudo apt-get -y install npm #Install NodeJs and its package manager
+sudo ln -s /usr/bin/nodejs/usr/bin/node # call node runs nodejs
+sudo npm install -g less less-plugin-clean-css #Install less compiler
+
+# wkhtmltopdf: Verificar si hace falta para Odoo 10.0
+
+# wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.1/wkhtmltox-0.12.1_linux-trusty-amd64.deb
